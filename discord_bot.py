4500c0b6ed9a6c1c329e53f111c02d08e670ea30
Markic01 <01marko.radosavljevic@gmail.com>
2024-09-24 22:39:51 +0200

    initial commit

---
+++ discord_bot.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+import os
+
+assert 'BOT_TOKEN' in os.environ, "BOT_TOKEN is not set in the environment variables."
+TOKEN = os.environ['BOT_TOKEN']
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot connected as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    user = ctx.message.author 
+    await ctx.send(f'Hello {user.name}!')
+
+bot.run(TOKEN)
